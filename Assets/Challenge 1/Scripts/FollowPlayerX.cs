9603a1bf3d2e258d8379e61e6f29fe976c8363b6
Holographic <budninja36@gmail.com>
2023-06-21 14:14:11 -0400

    ys

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    [SerializeField] private GameObject plane;
+    private Vector3 offset;
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
