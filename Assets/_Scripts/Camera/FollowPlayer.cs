9ed0b4807c428f755110656ca404c89dd605b6db
Holographic <budninja36@gmail.com>
2023-06-21 14:13:46 -0400

    Challenge 1

--- Assets/_Scripts/Camera/FollowPlayer.cs
+++ Assets/_Scripts/Camera/FollowPlayer.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField] private GameObject player;
     private Vector3 _offset = new Vector3(0, 5, -7);
 
     private void LateUpdate()
-    {
+    {   
+        // Offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + _offset;
     }
 }
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -3,21 +3,25 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed = 45;
+    private float _speed = 20;
+    private float _turnSpeed = 45;
     
-    public float horizontalInput;
-    public float forwardInput;
+    private float _horizontalInput;
+    private float _forwardInput;
     
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardInput = Input.GetAxis("Vertical");
         
         // Move the Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-    
-        // slide the vehicle left and right
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _forwardInput);
+        
+        // Turn the vehicle left and right
+        transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
+        
+        /* slide the vehicle left and right
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        */
     }
 }
